e81a793d1fd2ebd10fc4944ab5dd7162149eafdd
koshun-code <sit.com.k@gmail.com>
2022-10-17 17:29:31 +0300

    add option

--- gendiff.js
+++ gendiff.js
@@ -4,6 +4,7 @@ import { program } from 'commander';
 
 program
   .version('1.0')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f --format <type>', 'output format');
 
 program.parse();
