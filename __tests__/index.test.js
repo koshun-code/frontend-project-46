addbd6a78aa3fd59eaff638c89bf57f9cbd7bd91
koshun-code <sit.com.k@gmail.com>
2022-11-13 13:28:58 +0300

    change fixtures file name

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,11 +6,11 @@ import { makePath } from '../src/utils.js';
 const getFixturePath = (filename) => makePath(['__fixtures__', filename]);
 
 const dataStylish = [
-  ['/__fixtures__/file11.json', '/__fixtures__/file12.json', 'stylish.txt', 'stylish'],
+  ['/__fixtures__/file1.json', '/__fixtures__/file2.json', 'stylish.txt', 'stylish'],
   ['/__fixtures__/file1.yml', '/__fixtures__/file2.yml', 'stylish.txt', 'stylish'],
-  ['/__fixtures__/file11.json', '/__fixtures__/file12.json', 'plain.txt', 'plain'],
+  ['/__fixtures__/file1.json', '/__fixtures__/file2.json', 'plain.txt', 'plain'],
   ['/__fixtures__/file1.yml', '/__fixtures__/file2.yml', 'plain.txt', 'plain'],
-  ['/__fixtures__/file11.json', '/__fixtures__/file12.json', 'json.txt', 'json'],
+  ['/__fixtures__/file1.json', '/__fixtures__/file2.json', 'json.txt', 'json'],
   ['/__fixtures__/file1.yml', '/__fixtures__/file2.yml', 'json.txt', 'json'],
 ];
 test.each(dataStylish)('Compare %s with %s to be %s', (file1, file2, expected, format) => {
