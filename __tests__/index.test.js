04d7b12300c2cf14db8ef1801aa38c6abeb3437e
koshun-code <sit.com.k@gmail.com>
2022-11-13 13:11:29 +0300

    fix

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,11 +7,11 @@ const getFixturePath = (filename) => makePath(['__fixtures__', filename]);
 
 const dataStylish = [
   ['/__fixtures__/file11.json', '/__fixtures__/file12.json', 'stylish.txt', 'stylish'],
-  ['/__fixtures__/file11.yml', '/__fixtures__/file12.yml', 'stylish.txt', 'stylish'],
+  ['/__fixtures__/file1.yml', '/__fixtures__/file2.yml', 'stylish.txt', 'stylish'],
   ['/__fixtures__/file11.json', '/__fixtures__/file12.json', 'plain.txt', 'plain'],
-  ['/__fixtures__/file11.yml', '/__fixtures__/file12.yml', 'plain.txt', 'plain'],
+  ['/__fixtures__/file1.yml', '/__fixtures__/file2.yml', 'plain.txt', 'plain'],
   ['/__fixtures__/file11.json', '/__fixtures__/file12.json', 'json.txt', 'json'],
-  ['/__fixtures__/file11.yml', '/__fixtures__/file12.yml', 'json.txt', 'json'],
+  ['/__fixtures__/file1.yml', '/__fixtures__/file2.yml', 'json.txt', 'json'],
 ];
 test.each(dataStylish)('Compare %s with %s to be %s', (file1, file2, expected, format) => {
   expect(diff(file1, file2, format)).toEqual(fs.readFileSync(getFixturePath(expected), 'utf8'));
