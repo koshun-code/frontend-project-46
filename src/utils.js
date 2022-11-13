85a5f192174f929c3b9fa52659555bfa955983d0
koshun-code <sit.com.k@gmail.com>
2022-11-13 13:43:41 +0300

    resolve

--- src/utils.js
+++ src/utils.js
@@ -6,5 +6,5 @@ import fs from 'node:fs';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 export const makePath = (paths) => path.join(__dirname, '..', ...paths);
-export const getFile = (paths) => fs.readFileSync(makePath(paths));
+export const getFile = (paths) => fs.readFileSync(makePath(paths), 'utf-8');
 export const getExt = (fileName) => path.extname(fileName).slice(1);
